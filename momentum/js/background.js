7adbe2441c9e88be8b66b7cad45d442d4ef457d2
Profsquirrel <103481483+Profsquirrel@users.noreply.github.com>
2022-07-21 21:10:33 +0900

    Delete momentum directory

--- momentum/js/background.js
+++
@@ -1,6 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpg","3.jpg","4.jpg","5.jpg","6.jpg"];
-const chosenImage = images[Math.floor(Math.random()*images.length)];
-
-$('#wrapper').css({
-  'background-image':`url(images/${chosenImage})`
-});
